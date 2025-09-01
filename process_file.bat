fe360f8ac7044d43cde1f77faf0ae7f75ce19c1b
rogeliosamson <rogelio.samson@dfnn.com>
2025-09-01 10:58:12 +0800

    Update process_file.bat

--- process_file.bat
+++ process_file.bat
@@ -1,5 +1,5 @@
 @echo off
 cd \1play
 java -jar SftpProcessor-1.0.jar "PULL" "116.66.183.44" "instawin"  "JO5U6InO1yACmoA3RsWbOqMSOiQwWGr/TyEx1gXZz6aF2T45ggqqhWlsOPR+OttR"  "8722" "1play/output"  "c:/1Play/1Play-Reports"
-java -jar OnePlayReportParser-1.0.jar "c:/1Play/1Play-Reports" "c:/1Play/1Play-Reports/processed" "VENUE" "IW1PLAY" "IWLU1TER0C"
+java -jar OnePlayReportParser-1.0.jar "c:/1Play/1Play-Reports" "c:/1Play/1Play-Reports/processed" "VENUE"
 exit
